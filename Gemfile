116764e766d46744d31fbf597bc85e7e21fd6beb
Tommy Palmer <hi@tommyp.org>
2012-08-21 21:28:51 +0100

    adding coffee, sass and fixing the layout

--- Gemfile
+++ Gemfile
@@ -1,3 +1,8 @@
+source 'https://rubygems.org'
+
 gem 'bundler'
 gem 'sinatra'
 gem 'haml'
+gem 'sass'
+gem 'coffee-script'
+gem 'shotgun'
--- app.rb
+++ app.rb
@@ -6,3 +6,11 @@ set :public_folder, File.dirname(__FILE__) + '/public'
 get '/' do
   haml :index
 end
+
+get '/style.css' do
+  scss :style
+end
+
+get '/app.js' do
+  coffee :js
+end
